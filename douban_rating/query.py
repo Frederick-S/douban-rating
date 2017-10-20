2e69cfd8845f2eb31a82471966d75be0638e98b5
Frederick-S <mao_xiaodan@hotmail.com>
2017-10-20 20:03:49 +0800

    Run pep8

--- douban_rating/query.py
+++ douban_rating/query.py
@@ -6,11 +6,13 @@ from douban_rating.rating import Rating
 
 query_url = 'https://book.douban.com/j/subject_suggest?q={query}'
 
+
 def query(title):
     response = requests.get(query_url.format(query=title))
     items = response.json()
 
     return [get_rating(item) for item in items]
+
 
 def get_rating(item):
     document = PyQuery(url=item.get('url'))
